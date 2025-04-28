ca7c67c38c7622156b7644f11e21510765183dd2
Iffat-Nishat-IT22014 <it22014@mbstu.ac.bd>
2025-04-28 14:44:46 +0600

    Add files via upload

---
+++ encapsulation.java
@@ -0,0 +1,22 @@
+class Wallet {
+    private double money;
+
+    public void addMoney(double amount) {
+        if(amount > 0) {
+            money += amount;
+        }
+    }
+
+    public double checkMoney() {
+        return money;
+    }
+}
+
+public class encapsulation {
+    public static void main(String[] args) {
+        Wallet myWallet = new Wallet();
+        myWallet.addMoney(300);
+        System.out.println(myWallet.checkMoney());
+    }
+}
+
